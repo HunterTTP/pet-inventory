b2854cc42c01d8f3266ad99ebbfcbeb3ba1b2826
HunterTTP <HunterTTP@gmail.com>
2025-04-15 14:36:27 -0400

    Initial Fabric mod template build

---
+++ src/client/java/com/hunterttp/petinventory/PetInventoryClient.java
@@ -0,0 +1,9 @@
+package com.hunterttp.petinventory;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PetInventoryClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
