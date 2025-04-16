7ac9dc6aaf152b06d311a05ea0efac59c33ff18b
HunterTTP <HunterTTP@gmail.com>
2025-04-16 14:53:31 -0400

    Simplified inventory logic

--- src/main/java/com/hunterttp/petinventory/component/PetInventoryComponent.java
+++ src/main/java/com/hunterttp/petinventory/component/PetInventoryComponent.java
@@ -2,34 +2,29 @@ package com.hunterttp.petinventory.component;
 
 import net.minecraft.inventory.Inventories;
 import net.minecraft.inventory.SimpleInventory;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.registry.RegistryWrapper;
-import net.minecraft.util.collection.DefaultedList;
 import org.ladysnake.cca.api.v3.component.Component;
 
 public class PetInventoryComponent implements Component {
-    private final SimpleInventory inventory = new SimpleInventory(27);
+    private SimpleInventory inventory;
 
     public SimpleInventory getInventory() {
+        if (inventory == null) {
+            inventory = new SimpleInventory(27);
+        }
         return inventory;
     }
 
     @Override
     public void readFromNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
-        var items = DefaultedList.ofSize(inventory.size(), ItemStack.EMPTY);
-        Inventories.readNbt(tag, items, registryLookup);
-        for (int i = 0; i < items.size(); i++) {
-            inventory.setStack(i, items.get(i));
-        }
+        Inventories.readNbt(tag, getInventory().heldStacks, registryLookup);
     }
 
     @Override
     public void writeToNbt(NbtCompound tag, RegistryWrapper.WrapperLookup registryLookup) {
-        var items = DefaultedList.ofSize(inventory.size(), ItemStack.EMPTY);
-        for (int i = 0; i < items.size(); i++) {
-            items.set(i, inventory.getStack(i));
+        if (inventory != null) {
+            Inventories.writeNbt(tag, inventory.heldStacks, registryLookup);
         }
-        Inventories.writeNbt(tag, items, registryLookup);
     }
 }
